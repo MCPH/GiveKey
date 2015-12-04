b74b651e20c7d4f86abeae71beb98985d86b2a3d
Anton Magsino <MinecrafterPH@users.noreply.github.com>
2015-12-05 00:38:43 +0800

    Update Main.php

--- src/GK/Main.php
+++ src/GK/Main.php
@@ -12,31 +12,30 @@ use pocketmine\Server;
 use pocketmine\command\ConsoleCommandSender;
 class Main extends PluginBase implements Listener
 {
-public function onEnable() {
+	public function onEnable() {
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
 	
-		$this->getServer()->getPluginManager()->registerEvents($this,$this);
-		
+	public function onCommand(CommandSender $sender, Command $cmd, $label, array $args) {
+        	if(strtolower($cmd->getName()) === "givekey") {
+        		if($sender instanceof Player){
+        			if(isset($args[0])) {
+        			        $amount = $args[0];
+        			        foreach($args as $item){
+        					foreach($this->getServer()->getOnlinePlayers() as $p){
+        						$p->getInventory()->addItem(Item::get(Item::SLIMEBALL, 0, $item));
+        						$p->sendMessage($sender->getName()." gave you ".$item." CrateKeys!");
+        						return true;
+        				        }
+        			        }
+        		        }else{
+                			$sender->sendMessage("usage: /givekey (amount)");
+        		        }
+        	        }else{
+      				$sender->sendMessage("Please do this command in-game");
+      				return false;
+        		}
 		}
-		 public function onCommand(CommandSender $sender, Command $cmd, $label, array $args) {
-        if(strtolower($cmd->getName()) === "givekey") {
-        if($sender instanceof Player){
-        if(isset($args[0])) {
-                $amount = $args[0];
-                foreach($args as $item){
-        foreach($this->getServer()->getOnlinePlayers() as $p){
-        $p->getInventory()->addItem(Item::get(Item::SLIMEBALL,0,$item));
-        $p->sendMessage($sender->getName()." gave you ".$item." CrateKeys!");
-        return true;
-                }
-                }
-                }else{
-                $sender->sendMessage("usage: /givekey (amount)");
-                }
-                }else{
-      $sender->sendMessage("Please do this command in-game");
-      return false;
-                }
-		 }
-		 }
+	}
 }
               
